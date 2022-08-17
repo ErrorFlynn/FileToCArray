a31bf96845d3b77439c07ea5f837462271412967
Jordan Irwin <antumdeluge@gmail.com>
2022-08-16 18:32:57 -0700

    Resource file for embedding icon into executable

--- FileToCArray_main.cpp
+++ FileToCArray_main.cpp
@@ -1,8 +1,11 @@
 #include "FileToCArray.hpp"
 
-int main()
+int main(int argc, char** argv)
 {
 	ftca_form fm;
+#if defined(__WIN32) || defined(_MSC_VER)
+	nana::API::window_icon(fm.handle(), nana::paint::image(argv[0]));
+#endif
 	fm.show();
 	nana::exec();
 }
